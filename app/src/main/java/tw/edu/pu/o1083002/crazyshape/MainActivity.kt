bed133ebada272f522792ca6c1cc5d6541e340dc
student <o1083002@gm.pu.edu.tw>
2021-05-24 10:56:28 +0800

    2021上機考v0_(巫萬升)

--- app/src/main/java/tw/edu/pu/o1083002/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083002/crazyshape/MainActivity.kt
@@ -1,17 +1,14 @@
 package tw.edu.pu.o1083002.crazyshape
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
-import kotlinx.android.synthetic.main.activity_game.*
 
 @GlideModule
 public final class MyAppGlideModule : AppGlideModule()
@@ -22,13 +19,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val img:ImgTitle = findViewById(R.id.img)
+        //val img: ImageView = findViewById(R.id.imgTitle)
+        /*
         GlideApp.with(this)
-            .load(R.drawable.earth)
-            .into(img)
+                .load(R.drawable.cover)
+                .override(800*600)
+            .into(imgTitle)
+
+         */
 
 
-        Toast.makeText(baseContext,"作者：巫萬升",Toast.LENGTH_LONG).show()
+
+
+        Toast.makeText(baseContext,"作者：巫萬升 ",Toast.LENGTH_LONG).show()
 
         imgNext.setOnLongClickListener(object: View.OnLongClickListener{
                 override fun onLongClick(p0:View?):Boolean{

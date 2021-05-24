7f379fd6a961eda3fe73b220644306512a14b4b6
student <o1083002@gm.pu.edu.tw>
2021-05-24 09:51:03 +0800

    2021上機考v1_(巫萬升)

--- app/src/main/java/tw/edu/pu/o1083002/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083002/crazyshape/MainActivity.kt
@@ -9,12 +9,24 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_game.*
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img:ImgTitle = findViewById(R.id.img)
+        GlideApp.with(this)
+            .load(R.drawable.earth)
+            .into(img)
+
 
         Toast.makeText(baseContext,"作者：巫萬升",Toast.LENGTH_LONG).show()
 
